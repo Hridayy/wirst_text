df2b7d33559118710be4a5e316a90a9bd6c87eb9
Hridayy <73392185+Hridayy@users.noreply.github.com>
2022-04-17 12:22:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWristX=0
+rightWristX=0
+noseX=0
+noseY=0 
+difference=0
 function setup(){
     video = createCapture(VIDEO)
     video.size(550,500);
@@ -11,5 +16,20 @@ function setup(){
  }
  function gotposes(){
  if(results.length > 0){
- console.log(results)}
+ console.log(results)
+ noseX=results[0].pose.nose.x;
+ noseY=results[0].pose.nose.y;
+ console.log("NoseX ="+ noseX+ "NoseY ="+ noseY )
+ rightWristX=results[0].pose.rightWrist.x;
+ leftWristX=results[0].pose.leftWrist.x;
+ difference=floor(leftWristX-rightWristX)
+ console.log("leftwristX ="+leftWristX+"rightwirstX"+rightWristX+"difference="+difference)
+}
  }
+ 
+function draw(){
+    background("grey") 
+    document.getElementById("text_size").innerHTML="The Width And Height of Text will be:-"+difference+"px"
+    fill("orange")
+    text("Happy Hanuman Jayati",50,400)
+}
